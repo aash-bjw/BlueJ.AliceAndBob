d5ce5d0429941c17f3858b6db1c21c28344a8711
Aashna <bajwa.aashna@gmail.com>
2019-05-23 13:54:16 -0400

    Completed Alice & Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner stdin = new Scanner(System.in);
+        
+        String username = new String();
+     
+    System.out.println("What is your name?"); 
+    username = stdin.nextLine();
+    
+    if (username.equals("Alice") || username.equals("Bob")){
+        System.out.println("Hello, " + username +"!"); 
+    }
+      
     }
 }
